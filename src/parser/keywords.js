ee7dff982740613cc9efabdaf483afcc08e971db
Kaylimo <jp.turowicz@gmail.com>
2018-03-16 10:01:53 +0100

    Nouvelle branche avec code plus factorisé (tokenizer à réécrire)

--- src/parser/keywords.js
+++
@@ -1,8 +0,0 @@
-const keywords = {
-  'string-default_type': { r: /(String)/, s: 'String' },
-  'int-default_type': { r: /(int)/, s: 'int' },
-  'log_and-default_operator': { r: /(&&)/, s: '&&' },
-  'if-control_flow': { r: /(if)/, s: 'if' }
-};
-
-module.exports = keywords;
--- src/tokenizer/Token.js
+++ src/tokenizer/Token.js
@@ -8,4 +8,5 @@ class Token {
 	}
 }
 
-module.exports = Token;
+
+export default Token;
